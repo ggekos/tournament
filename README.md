72cacd70ef63b410c640ed43a577173b696795bb
Alexandre Colella <alexandre@quable.fr>
2017-03-20 17:02:33 +0100

    adding sf3

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# tournament
+tournament
+==========
+
+A Symfony project created on March 20, 2017, 2:00 pm.
--- test.php
+++ test.php
@@ -1,43 +1,58 @@
 <?php
-for ($i=0; $i < 1001; $i++) { 
-	$players['player'.$i] = rand(0,27);
+for ($i=0; $i < 1000; $i++) { 
+	$players['player'.$i] = 0;
 }
-$totalmatch = round((count($players)/2));
-$match = [];
 
 print 'Il y a '.count($players)." joueurs \r\n" ;
 print 'Ronde avec '.$totalmatch.' matchs'." \r\n" ;
+print howManyRound($players) . ' rondes'." \r\n" ;
+
+for ($i=0; $i < howManyRound($players); $i++) { 
+	print 'ronde '.$i." \r\n" ;
+
+	$matchs = createRound($players);
+
+	foreach ($matchs as $key => $value) {
+	if(rand(0,1)==0){
+		$players[$value[0]] += 3;
+	}else{
+		$players[$value[1]] += 3;
+	}
+	}
+}
 
 asort($players);
+var_dump($players);
 
-if(count($players)%2 == 1){
+function createRound(&$players){
+	$totalmatch = round((count($players)/2));
+	$match = [];
+
+	asort($players);
+
+	if(count($players)%2 == 1){
 	//test si le gars a pas deja eu un bye
 	$match[] = [key($players),'johny'];
-}
-$opponents = array_reverse($players);
-foreach (array_reverse($players) as $player => $point) {
-	if(alreadyInMatch($player, $match)){
-		continue;
 	}
-	$matchnotok = true;
-	$i=0;
-	reset($opponents);
-	while ($matchnotok != false) {
-		if(key($opponents)!= $player && !alreadyInMatch(key($opponents), $match)){
-			$match[]=[$player, key($opponents)];
-			$matchnotok = false ;
-		}
-		next($opponents);
-		$i++;
-	}
-}
+	$opponents = array_reverse($players);
 
-foreach ($match as $key => $mat) {
-	print "match $key ";
-	foreach ($mat as $player) {
-		print $player.' '.$players[$player].' ';
+	foreach (array_reverse($players) as $player => $point) {
+		if(alreadyInMatch($player, $match)){
+			continue;
+		}
+		$matchnotok = true;
+		$i=0;
+		reset($opponents);
+		while ($matchnotok != false) {
+			if(key($opponents)!= $player && !alreadyInMatch(key($opponents), $match)){
+				$match[]=[$player, key($opponents)];
+				$matchnotok = false ;
+			}
+			next($opponents);
+			$i++;
+		}
 	}
-	print "\r\n";
+	return $match;
 }
 
 function alreadyInMatch($player, $match){
@@ -51,5 +66,13 @@ function alreadyInMatch($player, $match){
 
 function alreadyMatch($player1, $player2, $history){
 	return false;
+}
+
+function howManyRound($players){
+	for ($i=1; $i < 50; $i++) { 
+		if(count($players) <= pow(2,$i)){
+			return $i ;
+		}
+	}
 }
 ?> 
